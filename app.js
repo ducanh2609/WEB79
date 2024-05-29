9b550efd44465fa5c30af938841ee3653a00f2c5
ducanh2609 <ducanh.nl2@gmail.com>
2024-05-29 22:29:07 +0700

    lession4

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-import express, { application } from 'express'
-// import { users, posts } from './data/data.js'
-import { getData, getId } from './ultis/constans.js'
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import UserModel from './models/users/UserModel.js'
+dotenv.config()
 
 
 const app = express()
@@ -11,6 +13,10 @@ app.use(express.json())
 app.get('/', () => {
     res.send('Homepage')
 })
+
+
+
+
 
 // app.get('/users/:id', (req, res) => {
 //     const { id } = req.params
@@ -152,142 +158,228 @@ app.get('/', () => {
 //     })
 // })
 
-app.post('/users', async (req, res) => {
-    const { userName } = req.body
-    console.log('userName', userName)
-    try {
-        const data = await getData('users')
-        const checkUser = data.find((item) => item.userName === userName)
-        if (checkUser) {
-            throw new Error('User exist')
-        }
-        data.push({
-            id: `US${Date.parse(new Date) / 1000}`,
-            userName,
-        })
-        res.status(200).send({
-            message: 'Successfully',
-            status: 200,
-        })
-    } catch (error) {
-        console.log(err)
-        res.status(405).send({
-            message: err.message,
-            status: 405,
-        })
-    }
-    // fetch('http://localhost:3000/users').then((rs) => {
-    //     return rs.json()
-    // }).then((data) => {
-    //     const checkUser = data.find((item) => item.userName === userName)
-    //     if (checkUser) {
-    //         throw new Error('User exist')
-    //     }
-    //     data.push({
-    //         id: `US${Date.parse(new Date) / 1000}`,
-    //         userName,
-    //     })
+// app.post('/users', async (req, res) => {
+//     const { userName } = req.body
+//     console.log('userName', userName)
+//     try {
+//         const data = await getData('users')
+//         const checkUser = data.find((item) => item.userName === userName)
+//         if (checkUser) {
+//             throw new Error('User exist')
+//         }
+//         data.push({
+//             id: `US${Date.parse(new Date) / 1000}`,
+//             userName,
+//         })
+//         res.status(200).send({
+//             message: 'Successfully',
+//             status: 200,
+//         })
+//     } catch (error) {
+//         console.log(err)
+//         res.status(405).send({
+//             message: err.message,
+//             status: 405,
+//         })
+//     }
+// fetch('http://localhost:3000/users').then((rs) => {
+//     return rs.json()
+// }).then((data) => {
+//     const checkUser = data.find((item) => item.userName === userName)
+//     if (checkUser) {
+//         throw new Error('User exist')
+//     }
+//     data.push({
+//         id: `US${Date.parse(new Date) / 1000}`,
+//         userName,
+//     })
 
-    //     res.status(200).send({
-    //         message: 'Successfully',
-    //         status: 200,
-    //     })
-    // }).catch(err => {
-    //     console.log(err)
-    //     res.status(405).send({
-    //         message: err.message,
-    //         status: 405,
-    //     })
-    // })
-})
+//     res.status(200).send({
+//         message: 'Successfully',
+//         status: 200,
+//     })
+// }).catch(err => {
+//     console.log(err)
+//     res.status(405).send({
+//         message: err.message,
+//         status: 405,
+//     })
+// })
+// })
 
-app.post('/posts', async (req, res) => {
-    const { authorId } = req.query
-    const { content } = req.body
+// app.post('/posts', async (req, res) => {
+//     const { authorId } = req.query
+//     const { content } = req.body
+//     try {
+//         const data = await getData('posts')
+//         const newData = {
+//             id: `PS${getId()}`,
+//             content,
+//             authorId,
+//         }
+//         data.push(newData)
+//         res.status(200).send({
+//             message: 'Successfully',
+//             data: newData,
+//         })
+//     } catch (error) {
+//         res.status(500).send({
+//             message: 'Internet server error',
+//             status: 500,
+//         })
+//     }
+// })
+
+// app.put('/posts/:postId', async (req, res) => {
+//     const { postId } = req.params
+//     const { authorId } = req.query
+//     const { content } = req.body
+//     try {
+//         const data = await getData('posts')
+//         const findPost = data.find((item) => item.id === postId)
+//         const indexPost = data.findIndex((item) => item.postId === postId)
+//         if (findPost) {
+//             const checkUser = findPost.authorId === authorId
+//             if (checkUser) {
+//                 const updatePost = {
+//                     ...findPost,
+//                     content,
+//                 }
+//                 data[indexPost] = updatePost
+//                 res.status(200).json({
+//                     message: 'Update successfully'
+//                 })
+//             } else {
+//                 throw new Error('User is incorrect')
+//             }
+//         } else {
+//             throw new Error('post not found')
+//         }
+//     } catch (error) {
+//         res.status(405).send({
+//             message: error.message,
+//             status: 405,
+//         })
+//     }
+// })
+
+// app.post('/comments/:postId', async (req, res) => {
+//     const { postId } = req.params
+//     const { authorId } = req.query
+//     const { content } = req.body
+//     try {
+//         const data = await getData('posts')
+//         const checkPost = data.find((item) => item.postId = postId)
+//         if (!checkPost) {
+//             throw new Error('post not found')
+//         }
+//         if (checkPost.authorId === authorId) {
+//             throw new Error('user can not comment to this post')
+//         }
+//         const newData = {
+//             id: `CMT${getId()}`,
+//             postId,
+//             content,
+//             authorId
+//         }
+//         data.push(newData)
+//         res.status(200).send({
+//             message: 'cmt successfully',
+//             status: 200,
+//         })
+//     } catch (error) {
+//         res.status(405).send({
+//             message: error.message,
+//             status: 405,
+//         })
+//     }
+// })
+
+// phương thức connect với tham số connect string
+
+app.post('/signin', async (req, res) => {
     try {
-        const data = await getData('posts')
-        const newData = {
-            id: `PS${getId()}`,
-            content,
-            authorId,
+        const { username, password } = req.body
+        const findUser = await UserModel.findOne({ username })
+        console.log('findUser', findUser)
+        if (findUser) {
+            throw new Error('Username is exist')
         }
-        data.push(newData)
-        res.status(200).send({
-            message: 'Successfully',
-            data: newData,
+        console.log('test')
+        const createdUser = await UserModel.create({
+            username, password
         })
+        if (createdUser) {
+            res.status(200).send({
+                message: 'Created',
+                data: createdUser,
+            })
+        }
     } catch (error) {
         res.status(500).send({
-            message: 'Internet server error',
-            status: 500,
+            error: error.message
         })
     }
 })
 
-app.put('/posts/:postId', async (req, res) => {
-    const { postId } = req.params
-    const { authorId } = req.query
-    const { content } = req.body
+app.get('/users', async (req, res) => {
     try {
-        const data = await getData('posts')
-        const findPost = data.find((item) => item.id === postId)
-        const indexPost = data.findIndex((item) => item.postId === postId)
-        if (findPost) {
-            const checkUser = findPost.authorId === authorId
-            if (checkUser) {
-                const updatePost = {
-                    ...findPost,
-                    content,
-                }
-                data[indexPost] = updatePost
-                res.status(200).json({
-                    message: 'Update successfully'
-                })
-            } else {
-                throw new Error('User is incorrect')
-            }
-        } else {
-            throw new Error('post not found')
-        }
-    } catch (error) {
-        res.status(405).send({
-            message: error.message,
-            status: 405,
-        })
-    }
-})
-
-app.post('/comments/:postId', async (req, res) => {
-    const { postId } = req.params
-    const { authorId } = req.query
-    const { content } = req.body
-    try {
-        const data = await getData('posts')
-        const checkPost = data.find((item) => item.postId = postId)
-        if (!checkPost) {
-            throw new Error('post not found')
-        }
-        if (checkPost.authorId === authorId) {
-            throw new Error('user can not comment to this post')
-        }
-        const newData = {
-            id: `CMT${getId()}`,
-            postId,
-            content,
-            authorId
-        }
-        data.push(newData)
+        const allUser = await UserModel.find()
         res.status(200).send({
-            message: 'cmt successfully',
-            status: 200,
+            users: allUser,
+            total: allUser.length,
         })
     } catch (error) {
-        res.status(405).send({
-            message: error.message,
-            status: 405,
+        res.send({
+            message: error.message
+        })
+    }
+
+})
+
+app.get('/users/:userId', async (req, res) => {
+    try {
+        const { userId } = req.params
+        const findUser = await UserModel.findById(userId)
+        if (!findUser) {
+            throw new Error('User not found')
+        }
+        res.status(200).send({
+            user: findUser,
+            status: 'Success'
+        })
+    } catch (error) {
+        res.send({
+            message: error.message
         })
     }
 })
+
+app.put('/users/:userId', async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const { password } = req.body;
+        const currentUser = await UserModel.findById(userId)
+        if (!currentUser) throw new Error('User is not exists!');
+        currentUser.password = password;
+        await currentUser.save();
+
+        res.status(201).send({
+            data: currentUser,
+            message: 'Updated info!',
+            success: true
+        });
+    } catch (error) {
+        res.status(403).send({
+            message: error.message,
+            data: null,
+            success: false
+        });
+    }
+})
+
+
+mongoose.connect(`mongodb+srv://${process.env.BD_USERNAME}:${process.env.BD_PASSWORD}@testdatabase.eg0demb.mongodb.net/`)
 
 
 app.listen(PORT, (err) => {
---
+++ models/users/UserSchema.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        required: true,
+        type: String,
+    },
+    password: {
+        required: true,
+        type: String,
+    },
+    role: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updateAt: {
+        type: Date,
+        default: Date.now()
+    },
+})
+
+// UserSchema.path('username').validate((value) => {
+//     if (value.length < 8) {
+//         this.invalidate('Phải lớn hơn 8 kí tự')
+//     }
+//     return true
+// })
+
+
+export default UserSchema
